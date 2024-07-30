3d365f31f1b427cbbcde3d1d17b7e8138c1be5a3
mohan <mohanreddy0532@gmail.com>
2024-07-30 13:19:25 +0530

    robshop-dev vpc subnets

--- main.tf
+++ main.tf
@@ -3,4 +3,8 @@ module "vpc" {
 
     cidr = var.vpc["cidr"]
     env  = var.env
+    public_subnets = var.vpc["public_subnets"]
+    app_subnets = var.vpc["app_subnets"]
+    web_subnets = var.vpc["web_subnets"]
+    db_subnets = var.vpc["db_subnets"]
 }

180602da41bafae51d4ac40450b3945ab2593e21
mohan <mohanreddy0532@gmail.com>
2024-07-30 13:41:55 +0530

    robshop-dev vpc 4 subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -39,3 +39,15 @@ resource "aws_subnet" "db" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.availability_zone[count.index]
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+

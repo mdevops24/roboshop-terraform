bc77c136e9d6e8a87331549fa2c9ea3ebffe55ce
mohan <mohanreddy0532@gmail.com>
2024-07-30 13:38:33 +0530

    robshop-dev vpc a.zones

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,13 +6,24 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
-  count      = length(var.public_subnets)
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
   availability_zone = var.availability_zone[count.index]
 
   tags = {
-    Name = "public-subnet"
+    Name = "web-subnet"
+  }
+}
+
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.availability_zone[count.index]
+
+  tags = {
+    Name = "web-subnet"
   }
 }

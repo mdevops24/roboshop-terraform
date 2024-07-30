105c231bfe01dc3202707564575877beada029d8
mohan <mohanreddy0532@gmail.com>
2024-07-30 13:25:19 +0530

    robshop-dev vpc subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,13 @@ resource "aws_vpc" "main" {
     Name = "${var.env}-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+
+  tags = {
+    Name = "public-subnet"
+  }
+}

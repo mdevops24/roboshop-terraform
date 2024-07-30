b335a99758ec2f2ff1f3cfbe81f507b508420310
mohan <mohanreddy0532@gmail.com>
2024-07-30 19:43:15 +0530

    robshop-dev create igw and add to rtb

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,6 +58,11 @@ resource "aws_route_table" "public" {
   count      =  length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.main.id
+    }
+
   tags = {
     Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
   }
@@ -115,3 +120,12 @@ resource "aws_route_table" "db" {
     subnet_id      = aws_subnet.db.*.id[count.index]
     route_table_id = aws_route_table.db.*.id[count.index]
   }
+
+## Internet gateway and add to abv rtb also
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}

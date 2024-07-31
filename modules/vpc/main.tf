c37df2f11d008fd021e4ef3654ef2349a4450ebe
mohan <mohanreddy0532@gmail.com>
2024-07-31 22:39:26 +0530

    ngw route add app,db

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -100,6 +100,11 @@ resource "aws_route_table" "db" {
   count      =  length(var.db_subnets)
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.*.id[count.index]
+  }
+
   tags = {
     Name = "db-rt-${split("-", var.availability_zones[count.index])[2]}"
   }

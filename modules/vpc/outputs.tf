df4e449d8d1a90ca6a0b3e27d66573d39f4f20df
mohan <mohanreddy0532@gmail.com>
2024-08-03 11:55:04 +0530

    ec2 updates

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,17 +6,24 @@ output  "public_subnet_ids" {
     value   = aws_subnet.public.*.ids
 }
 
-
 output  "app_subnet_ids" {
     value   = aws_subnet.app.*.ids
 }
-
 
 output  "web_subnet_ids" {
     value   = aws_subnet.web.*.ids
 }
 
-
 output  "db_subnet_ids" {
     value   = aws_subnet.db.*.ids
+}
+
+output  "subnets" {
+  value = tomap({
+     "web" = aws_subnet.web.*.id
+     "app" = aws_subnet.app.*.id
+     "db" = aws_subnet.db.*.id
+     "public" = aws_subnet.public.*.id
+
+     })
 }

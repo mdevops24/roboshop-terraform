d9bf128d1e361188af3b75f6f2b8b55ecb445093
mohan <mohanreddy0532@gmail.com>
2024-08-03 12:08:16 +0530

    ec2 updates

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,5 +1,5 @@
-name          = each.key
-instance_type = each.value["instance_type"]
-app_port      = each.value["app_port"]
-app_sg_cidr   = each.value["app_sg_cidr"]
-subnet        = module.vpc.subnets["web"][0]
+variable "name" {}
+variable "instance_type" {}
+variable "app_port" {}
+variable "app_sg_cidr" {}
+variable "subnet" {}

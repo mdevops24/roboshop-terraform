19577a306322c6759036248cd652776419f452ee
mohan <mohanreddy0532@gmail.com>
2024-08-03 15:50:32 +0530

    ec2 awe_auto_scaling apps

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ module "vpc" {
     default_vpc_cidr   = var.vpc["default_vpc_cidr"]
 }
 
-module "ec2" {
+module "apps" {
     source= "./modules/ec2"
 
-    for_each = var.ec2
-    name     = each.key
-    instance_type = each.value["instance_type"]
+    for_each        = var.ec2
+    name            = each.key
+    instance_type   = each.value["instance_type"]
     allow_port      = each.value["app_port"]
     allow_sg_cidr   = each.value["app_sg_cidr"]
     subnet_ids      = module.vpc.subnets[each.value["subnet_ref"]]
